d3e9b81be6ef34232fb370adcea22a625a553eed
Aryansh1520 <aryan152015@gmail.com>
2024-07-10 14:42:59 +0530

    add globe

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
-import { FlipWordsDemo } from './components/elements/Flipwords.jsx'; // name of the function in elements/Flipwords.jsx
+import { FlipWordsDemo } from './components/elements/Flipwords.jsx';
+import { EvervaultCardDemo } from './components/elements/Evervault.jsx';
+import { GlobeDemo } from './components/elements/Globe.jsx';
+
 function App() {
   return (
-    <div>
-        <FlipWordsDemo/>
+    <div className="bg-black relative min-h-screen"> {/* Ensure full height and relative positioning */}
+        <div className="absolute inset-0 z-0"> {/* GlobeDemo as background */}
+            <GlobeDemo/>
+        </div>
+        <div className="z-10 relative"> {/* Content on top of the background */}
+            <FlipWordsDemo/>
+            <div className="flex justify-between"> {/* Adjusted for even spacing */}
+                <EvervaultCardDemo/>
+                <EvervaultCardDemo/>
+                <EvervaultCardDemo/>
+                <EvervaultCardDemo/>
+            </div>
+        </div>
     </div>
   );
 }
