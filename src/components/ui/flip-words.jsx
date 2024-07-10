0f782499aec370be2faf51c074f803d28b2710b0
Aryan Sharma <81407258+Aryansh1520@users.noreply.github.com>
2024-07-10 13:32:58 +0530

    Update flip-words.jsx

--- src/components/ui/flip-words.jsx
+++ src/components/ui/flip-words.jsx
@@ -1,7 +1,8 @@
 "use client";
 import React, { useCallback, useEffect, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
-import { cn } from '../../utils/cn.js';export const FlipWords = ({
+import { cn } from '../../utils/cn.js';
+export const FlipWords = ({
   words,
   duration = 3000,
   className,
